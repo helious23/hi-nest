3dc474bca5d8b3a63769d6bd9a7db0cb033d9d3a
Max16 <max16@naver.com>
2021-07-02 15:54:47 +0900

    #2.3 Movies Service part Two

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -9,7 +9,11 @@ export class MoviesService {
     return this.movies;
   }
   getOne(id: string): Movie {
-    return this.movies.find((movie) => movie.id === +id);
+    const movie = this.movies.find((movie) => movie.id === +id);
+    if (!movie) {
+      throw new NotFoundException(`Movie with ID ${id} not found.`);
+    }
+    return movie;
   }
   create(movidData) {
     this.movies.push({
@@ -17,8 +21,13 @@ export class MoviesService {
       ...movidData,
     });
   }
-  deleteOne(id: string): boolean {
-    this.movies.filter((movie) => movie.id !== +id);
-    return true;
+  deleteOne(id: string) {
+    this.getOne(id);
+    this.movies = this.movies.filter((movie) => movie.id !== +id);
+  }
+  update(id: string, updateData) {
+    const movie = this.getOne(id);
+    this.deleteOne(id);
+    this.movies.push({ ...movie, ...updateData });
   }
 }
